85ed12da44bef93d6a4a81c22724f3315ddeef68
Andrey Kislyuk <kislyuk@gmail.com>
2018-09-03 17:24:02 -0700

    v0.2.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require = ["coverage", "flake8", "wheel"]
 
 setup(
     name='gs',
-    version='0.2.4',
+    version='0.2.5',
     url='https://github.com/kislyuk/gs',
     license='Apache Software License',
     author='Andrey Kislyuk',
