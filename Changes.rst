12b6b0f5d950d5a0c8dc1b0d8514d750a3a8ef4d
Andrey Kislyuk <kislyuk@gmail.com>
2018-09-12 16:02:24 -0700

    v0.3.2

--- Changes.rst
+++ Changes.rst
@@ -1,3 +1,8 @@
+Changes for v0.3.2 (2018-09-12)
+===============================
+
+-  Avoid crashing when config save fails
+
 Changes for v0.3.1 (2018-09-12)
 ===============================
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require = ["coverage", "flake8", "wheel"]
 
 setup(
     name='gs',
-    version='0.3.1',
+    version='0.3.2',
     url='https://github.com/kislyuk/gs',
     license='Apache Software License',
     author='Andrey Kislyuk',
