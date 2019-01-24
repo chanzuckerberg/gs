48c995100005bac69fb00d072bf6d0175b8ae00f
Andrey Kislyuk <kislyuk@gmail.com>
2019-01-24 12:10:16 -0800

    Update release script

--- common.mk
+++ common.mk
@@ -1,5 +1,3 @@
-# Dependencies: git pandoc moreutils httpie
-
 SHELL=/bin/bash -eo pipefail
 
 release_major:
@@ -16,9 +14,10 @@ release_patch:
 
 release:
 	@if [[ -z $$TAG ]]; then echo "Use release_{major,minor,patch}"; exit 1; fi
-	@if ! which http; then echo "httpie is required. Please run pip install httpie"; exit 1; fi
-	@if ! which sponge; then echo "sponge is required. Please install moreutils"; exit 1; fi
-	@if ! which pandoc; then echo "Pandoc is required. Please run pip install pandoc"; exit 1; fi
+	@if ! type -P pandoc; then echo "Please install pandoc"; exit 1; fi
+	@if ! type -P sponge; then echo "Please install moreutils"; exit 1; fi
+	@if ! type -P http; then echo "Please install httpie"; exit 1; fi
+	@if ! type -P twine; then echo "Please install twine"; exit 1; fi
 	$(eval REMOTE=$(shell git remote get-url origin | perl -ne '/([^\/\:]+\/.+?)(\.git)?$$/; print $$1'))
 	$(eval GIT_USER=$(shell git config --get user.email))
 	$(eval GH_AUTH=$(shell if grep -q '@github.com' ~/.git-credentials; then echo $$(grep '@github.com' ~/.git-credentials | python3 -c 'import sys, urllib.parse as p; print(p.urlparse(sys.stdin.read()).netloc.split("@")[0])'); else echo $(GIT_USER); fi))
@@ -35,7 +34,7 @@ release:
 	    if [[ -f Changes.md ]]; then cat $$TAG_MSG <(echo) Changes.md | sponge Changes.md; git add Changes.md; fi; \
 	    if [[ -f Changes.rst ]]; then cat <(pandoc --from markdown --to rst $$TAG_MSG) <(echo) Changes.rst | sponge Changes.rst; git add Changes.rst; fi; \
 	    git commit -m ${TAG}; \
-	    git tag --annotate --file $$TAG_MSG ${TAG}
+	    git tag --sign --annotate --file $$TAG_MSG ${TAG}
 	git push --follow-tags
 	http --check-status --auth ${GH_AUTH} ${RELEASES_API} tag_name=${TAG} name=${TAG} \
 	    body="$$(git tag --list ${TAG} -n99 | perl -pe 's/^\S+\s*// if $$. == 1' | sed 's/^\s\s\s\s//')"
@@ -45,6 +44,7 @@ release:
 	$(MAKE) pypi_release
 
 pypi_release:
-	python setup.py sdist bdist_wheel upload
+	python setup.py sdist bdist_wheel
+	twine upload dist/*.tar.gz dist/*.whl --sign --verbose
 
 .PHONY: release
